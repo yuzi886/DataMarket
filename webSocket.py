0f6e680426b04d090b1e9ca0bbd742f4e90299ba
CSImage <18616312526@163.com>
2022-10-24 18:04:47 +0100

    realize the transform xml file by websocket

--- webSocket.py
+++ webSocket.py
@@ -1,35 +1,47 @@
 #!/usr/bin/env python
 
 import asyncio
- 
 import websockets
+from bs4 import BeautifulSoup
  
 
 def createFile(name,lines):
     try:
         fp = open(name, 'w')
-        for line in lines:
-            fp.write(str(line, 'utf-8'))
+        fp.write(lines)
         fp.close()
     except Exception as e:
         print(e)
+
+def count(lines):# count the number of record in the xml file
+    try:
+        line = lines.split('\n')
+        row = line[2].split()
+        tag = row[0].strip('>').strip('<') 
+        Bs_data = BeautifulSoup(lines, "xml")
+        record = Bs_data.find_all(tag)
+        return len(record)
+    except Exception as e:
+        print(e)
+    
 
 # create transformation for each connection
 async def get_stream(websocket, path):
     data = await websocket.recv()
     print(data)
-    result = []
+    result = ""
     try:
         while True:
             contents = await websocket.recv()
-            
-            print(str(contents, 'utf-8'))
-            if contents == bytes("end",'utf-8'):
+            contents = str(contents, 'utf-8')
+            print(contents)
+            if contents == "end":
+                print("the number of record is "+str(count(result)))
                 createFile('example.xml',result) 
                 # the name of file should be decided by the user
                 reply = f"The transform ends successfully"
                 await websocket.send(reply)
-            result.append(contents)
+            result += contents
     except:
         print("Client disconnected")   
 
