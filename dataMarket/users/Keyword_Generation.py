b655814b05c8aa9dfc93acc68a6af8a7b7940e65
CSImage <18616312526@163.com>
2022-12-06 01:50:33 +0000

    add synonyms, double search bar and column detail

--- dataMarket/users/Keyword_Generation.py
+++ dataMarket/users/Keyword_Generation.py
@@ -4,26 +4,40 @@ import nltk
 from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from collections import Counter
+from nltk.corpus import wordnet
 
 stop_word = set(stopwords.words('english'))
 ps = PorterStemmer()
 
 #extact the key word from the sentence and output dict_keys
 def extact(sentence):
-  #Romove non-letter characters
+  #lower case of whole sentence
+  sentence = sentence.casefold()
   word_list = sentence.split()
   regex = re.compile('[^a-zA-Z]')
   i = 0
   for item in word_list:
+       #Romove non-letter characters to be ""
        word_list[i] = regex.sub('', item)
        item = word_list[i]
        if item in stop_word:
               word_list[i] = ""
        else:
-              word_list[i] = ps.stem(item.casefold())
+              word_list[i] = ps.stem(item)
        i += 1
-  result = list(Counter(word_list))
+  result = list(filter(None, word_list)) #delete all none element
+  #result = list(Counter(word_list))
+
   if '' in result:
     result.remove('')
   return result
+
+def synonyms(word):
+       #get the synonyms of the word
+       synonyms = []
+       for syn in wordnet.synsets(word):
+           for lm in syn.lemmas():
+                    synonyms.append(lm.name())
+       return synonyms[:10]
+       
 
--- dataMarket/users/views.py
+++ dataMarket/users/views.py
@@ -6,18 +6,69 @@ from django.urls import reverse
 from datetime import datetime 
 import operator
 from . import Keyword_Generation as kg
-
+import json
+from django.contrib import messages
 
 data = None
-keyword_set = {}
-IDS = {} #store matching level between searching word and all dataset
+#keyword_set = {}
+keyword__domain = {}
+#IDS = {} #store matching level between searching word and all dataset
+domain =""
+
+def search(kw):
+	global data
+	keyword_set = {}
+	for d in data:
+		sentence = ''
+		for col_name in list(d.column_summary.keys()):
+			sentence = sentence + col_name +" "
+		keywords = kg.extact(sentence)
+		for k in keywords: # get every keyword from the the database
+			if k in keyword_set:# if the keyword exist
+				if d.id in keyword_set[k]:
+					#if the keyword exist and id already recorded, the frequency increase
+					keyword_set[k][d.id]+=1
+				else:
+					#if the keyword exist and id isn't recorded, record it 
+					keyword_set[k][d.id] = 1
+			else:
+				keyword_set[k] = {d.id:1}
+
+
+		# processed input query and displays candidate datasets
+		IDS = {}
+		#  keyword_set = keyword__domain[domain]
+		search_word_set = kg.extact(kw)
+		for w in search_word_set:
+			if w in keyword_set:
+				for item in keyword_set[w].items():
+					if item[0] in IDS:
+						IDS[item[0]]+= item[1]
+					else:
+						IDS[item[0]] = item[1]
+			else:
+				syn_list = kg.synonyms(w)
+				for syn in syn_list:
+					if syn in keyword_set:
+						for item in keyword_set[syn].items():
+							if item[0] in IDS:
+								IDS[item[0]]+= item[1]
+							else:
+								IDS[item[0]] = item[1]
+			
+		#sort the dataset by the mathching level
+		IDS = dict(sorted(IDS.items(), key=lambda x:x[1], reverse=True))
+		data = Dataset.objects.filter(id__in=list(IDS.keys()))
+
+
 def index(request):
-	global data,keyword_set,IDS
+	global data,domain,keyword__domain
 	context ={}
 	if request.method == 'POST':
 		domain = request.POST.get('domain','')
 		sort = request.POST.get('sort','')
-		search = request.POST.get('search','')
+		search1 = request.POST.get('search1','')
+		search2 = request.POST.get('search2','')
 		if domain != '':#for get all domain name 
 			domain_id = Domain.objects.filter(name=domain).values()
 			data = Dataset.objects.filter(domain_id = domain_id[0]['id'])
@@ -28,35 +79,67 @@ def index(request):
 			elif sort == 'price_h_l':
 				data = sorted(data, key=operator.attrgetter('price'),reverse=True)
 			else:
-				data = sorted(data, key=operator.attrgetter('user_id'))
+				data = sorted(data, key=operator.attrgetter('pub_by'))
 				#do not do sort by username(because l do not add user name)
 			context["metadata"]=data
-		elif search != ''and data != None:#use keyword to search data
-			if len(keyword_set) == 0:
-				dataset = Dataset.objects.all()
-				for d in dataset:
+		elif search1 != ''and data != None:
+			#seach algorithms 
+			if domain not in keyword__domain:
+				keyword_set = {}
+			#if len(keyword_set) == 0:
+				#dataset = Dataset.objects.all()
+				for d in data:
 					sentence = ''
 					#do not add seller name in sentence, because not store user name
-					sentence = sentence + d.title+" "+d.description+" "
+					sentence = sentence + d.title+" "+d.description+" "+ d.pub_by+" "
 					keywords = kg.extact(sentence)
-					for k in keywords:
-						if k in keyword_set:
-							keyword_set[k].append(d.id)
+					for k in keywords: # get every keyword from the the database
+						if k in keyword_set:# if the keyword exist
+							if d.id in keyword_set[k]:
+								#if the keyword exist and id already recorded, the frequency increase
+								keyword_set[k][d.id]+=1
+							else:
+								#if the keyword exist and id isn't recorded, record it 
+								keyword_set[k][d.id] = 1
 						else:
-							keyword_set[k] = [d.id]
-					IDS[d.id] = 0
-				print(keyword_set)
+							keyword_set[k] = {d.id:1}
+					#IDS[d.id] = 0
+				keyword__domain[domain] = keyword_set
+				#print(keyword_set)
 
-			search_word_set = kg.extact(search)
+
+			# processed input query and displays candidate datasets
+			IDS = {}
+			keyword_set = keyword__domain[domain]
+			search_word_set = kg.extact(search1)
 			for w in search_word_set:
 				if w in keyword_set:
-					for i in keyword_set[w]:
-						IDS[i]+=1
-			print(IDS)
-			IDS = sorted(IDS,reverse=True)
-			print(IDS)
-			data = sorted(data, key=lambda x: IDS.index(x.id))
+					for item in keyword_set[w].items():
+						if item[0] in IDS:
+							IDS[item[0]]+= item[1]
+						else:
+							IDS[item[0]] = item[1]
+				else:
+					syn_list = kg.synonyms(w)
+					for syn in syn_list:
+						if syn in keyword_set:
+							for item in keyword_set[syn].items():
+								if item[0] in IDS:
+									IDS[item[0]]+= item[1]
+								else:
+									IDS[item[0]] = item[1]
+			
+			#sort the dataset by the mathching level
+			IDS = dict(sorted(IDS.items(), key=lambda x:x[1], reverse=True))
+			data = Dataset.objects.filter(id__in=list(IDS.keys()))
+			if search2 != '' :
+				search(search2)
 			context["metadata"]=data
+		elif search2 != '' and search1 == '':
+			 messages.error(request, 'type in keywords of title, description,seller name firstly') 
+
+
+
 
 
 
@@ -68,3 +151,16 @@ def index(request):
 	context["domains"]=domains
 	template = loader.get_template('first.html')
 	return HttpResponse(template.render(context, request))
+
+
+def detail(request,ID):
+	context ={}
+	data = Dataset.objects.filter(id = ID)
+	for d in data:
+		#print(d.column_summary)
+		#data_dict = json.loads(d.column_summary)
+		context["column"] = d.column_summary
+
+	context["metadata"]=data
+	template = loader.get_template('dataList.html')
+	return HttpResponse(template.render(context,request))
